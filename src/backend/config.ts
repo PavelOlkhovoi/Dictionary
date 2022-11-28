43bbe64e32fef196ecc9a7fc0f290f3c2ea4a6d3
Pavel <p-olhovoi@bk.ru>
2022-11-28 23:06:59 +0100

    feat: Check the active user

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,12 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_APi,
   authDomain: process.env.REACT_APP_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -15,7 +8,4 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMEND_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './backend/config';
 
-console.log("Hi", process.env.REACT_APP_TEST)
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+console.log('Initialize')
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
