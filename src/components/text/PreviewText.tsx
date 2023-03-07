1de2e6eb51a22aa510e46cd2d17035e89d91aa25
Pavel <p-olhovoi@bk.ru>
2023-03-07 15:40:31 +0100

    refactoring: Add the new preview component of texts and sets

--- src/components/text/PreviewText.tsx
+++ src/components/text/PreviewText.tsx
@@ -1,39 +1,16 @@
-import { Link } from 'react-router-dom';
 import { Text } from '../../pages/types/word';
-import { styleTW } from '../../style';
-import LineButton from '../ui-elements/buttons\'/LineButton';
+import CardPreview from '../ui-elements/cards/CardPreview';
 
 interface Props {
     text: Text
 }
 const PreviewText = ({text}:Props) => {
-    const preview = text.text.split(' ').filter((t, ids) => ids < 30)
-    const wordsQuantity = text.wordsIds.length
+    const link = `/texts/${text.textId}`
+    
     return (
-        <div className='bg-white shadow px-8 py-6 cursor-pointer border-b-2 pb-2'>
-            <Link to={{pathname: `/texts/${text.textId}`}} className="flex flex-col h-full">
-            <h3 className={`${styleTW.title3} mb-2 border-b-2 pb-2 border-gray-200`}>{text.title}</h3>
-            <div className='mb-2 text-sm font-medium text-gray-700'>Used words {wordsQuantity}</div>
-            <div className='my-10'>
-            {
-                preview.map((t, ids) => {
-                    if(t === " "){
-                        return ''
-                    }
-                    const clearText = t.trim().replace('.', '')
-                    return preview.length -1 === ids ? <span key={ids}>{clearText}...</span> : <span key={ids}>{clearText} </span>
-                })
-            }
-            </div>
-            <div className='font-medium mt-auto flex justify-between items-baseline mt-auto mb-4'>
-                <div className='text-sm font-medium text-gray-700'>12 day ago</div>
-                <div>
-                    <LineButton>Read</LineButton>
-                </div>
-            </div>
-            </Link>
-        </div>
+        <CardPreview title={text.title} link={link} wordsIds={text.wordsIds} content={text.text} />
     )
 }
+
 
 export default PreviewText;
---
+++ src/components/ui-elements/cards/CardPreview.tsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom"
+import { styleTW } from "../../../style"
+import LineButton from "../buttons'/LineButton"
+import { useAppSelector } from "../../../hooks/redux-hooks"
+import { selectAllWordsIdsInArr, selectWordsArrById } from "../../../store/slices/wordSlice"
+import { useEffect } from "react"
+import TextHighlighter from "../../text/TextHighlighter"
+import { WordDb } from "../../../pages/types/word"
+
+interface Props {
+    link: string
+    wordsIds: string[]
+    date?: string
+    content: string
+    title: string
+}
+const CardPreview = ({link, wordsIds, content, title}: Props) => {
+    const wordsQuantity = wordsIds.length
+    const shortContent = content.trim().split(' ').filter((t, ids) => ids < 30)
+    const preview = shortContent.map((t,id) => {
+        const lastWord = shortContent[shortContent.length-1]
+        if(lastWord === t){ t.replace('.', '')
+            return `${t}...`
+        }
+        return t
+    }).join(' ')
+
+    const fullWords = useAppSelector(state => selectWordsArrById(state, wordsIds))
+
+    return (
+        <div className='bg-white shadow px-8 py-6 cursor-pointer border-b-2 pb-2'>
+            <Link to={link} className="flex flex-col h-full">
+            <h3 className={`${styleTW.title3} mb-2 border-b-2 pb-2 border-gray-200`}>{title}</h3>
+            <div className='mb-2 text-sm font-medium text-gray-700'>Used words {wordsQuantity}</div>
+            <div className='my-10'>
+            {
+                <TextHighlighter words={fullWords as WordDb[]} text={preview} wordsBack={()=>console.log('12345')} />
+            }
+            </div>
+            <div className='font-medium mt-auto flex justify-between items-baseline mt-auto mb-4'>
+                <div className='text-sm font-medium text-gray-700'>12 day ago</div>
+                <div>
+                    <LineButton>Read</LineButton>
+                </div>
+            </div>
+            </Link>
+        </div>
+    )
+}
+
+
+export default CardPreview;
