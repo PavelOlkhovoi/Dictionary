d5f56141837837b88dd8a2f54384d19f9cb54404
Pavel <p-olhovoi@bk.ru>
2023-03-19 22:30:07 +0100

    feat: Delete and update a translation fields

--- src/pages/words/wordsSteps/uiFields/TranslationFields.tsx
+++ src/pages/words/wordsSteps/uiFields/TranslationFields.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import LineButton from "../../../../components/ui-elements/buttons/LineButton";
 import MyInput from "../../../../components/wordsForm/ui/MyInput";
 import useInput from "../../../../hooks/useInput";
+import { styleTW } from "../../../../style";
 import { PartOfSpeechSelect } from "../../../types/word";
 import { WordForm } from "../AddWordsWithSteps";
 import { TranslationGroup } from "./TranslationGroup";
@@ -15,7 +16,7 @@ interface Props {
 }
 
 const TranslationFields = ({groupState, groupId, changeTranslationGroup}: Props) => {
-    const [meanings, setMeaning] = useState([{
+    const [meanings, setMeanings] = useState([{
         id: nanoid(),
         name: '',
         show: true
@@ -32,7 +33,7 @@ const TranslationFields = ({groupState, groupId, changeTranslationGroup}: Props)
             return m
         })
 
-        setMeaning(newState)
+        setMeanings(newState)
     }
 
     const handleOnBlur = () => {
@@ -41,23 +42,37 @@ const TranslationFields = ({groupState, groupId, changeTranslationGroup}: Props)
             if(g.id === groupId){
                 g.translation = cleanTranslation
             }
-
             return g
         })
         changeTranslationGroup(targetGroup)
     }
+    const deleteHandler = (id: string) => {
+        const newState = meanings.map(t => {
+            if(t.id === id){t.show = false}
+            return t
+        })
+
+        setMeanings(newState)
+    }
     return (
         <div className="py-4">
-        {
-            meanings.map(m => <MyInput key={m.id} 
+
+            {
+            meanings.map(m => {
+                return m.show && <div key={m.id} className="flex items-start">
+                <MyInput 
                 name="translation" label="translation" 
                 value={m.name} onChange={(e) => handleMeaning(m.id, e.target.value)}
                 onBlur={handleOnBlur}
-                />)
-        }
-        <div>
-            <LineButton onClick={()=> setMeaning(prev => [...prev, {id: nanoid(), name: '', show: true}])}>New translation</LineButton>
-        </div>
+                />
+                <div className={`${styleTW.bageRed}`} onClick={()=> deleteHandler(m.id)}>X</div>
+                </div> 
+                })
+            }
+            
+            <div>
+                <LineButton onClick={()=> setMeanings(prev => [...prev, {id: nanoid(), name: '', show: true}])}>New translation</LineButton>
+            </div>
         </div>
     )
 }
