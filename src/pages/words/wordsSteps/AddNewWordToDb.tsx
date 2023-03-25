4fb7e9bcd50b5ab5981699ec48d4c9947acdc596
Pavel <p-olhovoi@bk.ru>
2023-03-25 11:05:21 +0100

    feat: Add new tags and update old one

--- src/pages/words/wordsSteps/AddNewWordToDb.tsx
+++ src/pages/words/wordsSteps/AddNewWordToDb.tsx
@@ -53,7 +53,7 @@ const AddNewWordToDb = ({wordFields, changeWordSate}: Props) => {
 
             setDoc(docRef, {wordId: docRef.id}, { merge: true })
 
-            // addTags(docRef.id)
+            addTags(docRef.id)
 
             if(docRef.id){
                 changeWordSate(prev =>({
@@ -89,33 +89,17 @@ const AddNewWordToDb = ({wordFields, changeWordSate}: Props) => {
           }
     }
 
-//     const addTags = async (wordIdx: string) => {
+    const addTags = async (wordIdx: string) => {
 
-//       let isOldTag = false
+      wordFields.tags.addedTags.length > 0 && 
+      wordFields.tags.addedTags.filter(t => t.show).forEach(t => addWordIdxToTag(t.tagId, wordIdx))
 
-//       tags?.forEach(tagField => {
-//         if((oldTags as DocumentData).length === 0){
-//           createTag(user?.uid as string, tagField, wordIdx)
-//         }
-//         oldTags?.forEach((tag, idx) => {
-//           console.log('Add Tag')
-//           if(tagField === tag.name){
-//             isOldTag = true
-//             return addWordIdxToTag(tag.tagId, wordIdx)
-//           }
-          
-//           if(oldTags.length-1 === idx && !isOldTag){
-//             createTag(user?.uid as string, tagField, wordIdx)
-//           };
+      const newTags = wordFields.tags.newTags.filter(t => t.show).filter(t => t.name !== '')
 
-//           if(oldTags.length-1 === idx && isOldTag){
-//             isOldTag = !isOldTag
-//           };
-//         })
-//       }
-//     )
-      
-//   }
+      newTags.length > 0 && newTags.forEach(t => 
+        createTag(userId?.uid as string, t.name, wordIdx))
+
+  }
 
   useEffect(() => {
     wordFields.sendingData &&
